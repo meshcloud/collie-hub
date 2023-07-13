13781e18421787cdf1b4864aa10961ce60f258ea
Johannes Rudolph <jrudolph@meshcloud.io>
2023-07-13 21:04:11 +0200

    chore: use terraform to manage amplify app

---
+++ amplify/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.65.0"
+    }
+  }
+}
+
+provider "aws" {
+  region              = "eu-central-1"
+  allowed_account_ids = ["125191384046"]
+}
+
+resource "aws_amplify_app" "colliehub" {
+  name       = "collie-hub"
+  repository = "https://github.com/meshcloud/collie-hub"
+
+  custom_rule {
+    source = "/<*>"
+    status = "404-200"
+    target = "/index.html"
+  }
+  
+  // this is a default redirect set up by amplify
+  # custom_rule {
+  #   source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>" 
+  #   status = "200" 
+  #   target = "/index.html" 
+  # }
+}
+
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.colliehub.id
+  branch_name = "main"
+  framework   = "Web"
+  stage       = "PRODUCTION"
+  tags        = {}
+  
+}
+
+
+resource "aws_amplify_domain_association" "collie_cloudfoundation_org" {
+  app_id      = aws_amplify_app.colliehub.id
+  domain_name = "collie.cloudfoundation.org"
+  wait_for_verification = false
+  
+  sub_domain {
+    branch_name = aws_amplify_branch.main.branch_name
+    prefix      = ""
+  }
+}
+
+output "domains" {
+  value = [
+    aws_amplify_domain_association.collie_cloudfoundation_org
+  ]
+}
