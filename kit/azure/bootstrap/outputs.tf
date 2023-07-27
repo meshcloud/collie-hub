071c835c8f37437943768c4c1b997bbe017be567
Florian <flonowa@outlook.com>
2023-07-27 15:38:42 +0200

    chore: add outputs to bootstrap beccause dependency to module.hcl

---
+++ kit/azure/bootstrap/outputs.tf
@@ -0,0 +1,9 @@
+output "client_id" {
+  value = azuread_service_principal.cloudfoundation_deploy.application_id
+}
+
+output "client_secret" {
+  value     = azuread_service_principal_password.cloudfoundation_deploy.value
+  sensitive = true
+}
+	
