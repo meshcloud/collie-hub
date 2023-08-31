b620bada61df567bf2ce6831b2d5727ad900f89f
Johannes Rudolph <jrudolph@meshcloud.io>
2023-08-31 18:23:42 +0200

    feat: remove azure policy modules
    these will end up in landing zone modules in the near future,
    so there's no point fixing terraform validate errors here

--- kit/azure/policy/allowed-locations/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "allowed_locations" {
-  type        = list(string)
-  description = "Allowed Azure regions."
-}
-variable "parent_mg_id" {
-  type        = string
-  description = "parent folder ID"
-}
