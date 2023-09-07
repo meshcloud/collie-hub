84931ad5a250aee00c227377168e075918333e88
Florian <flonowa@outlook.com>
2023-09-07 11:31:48 +0200

    chore: add kit structure lz-serverless

--- kit/azure/landingzones/lz-serverless/main.tf
+++ kit/azure/landingzones/lz-serverless/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_management_group" "serverless" {
   display_name               = var.lz-serverless
-  parent_management_group_id = var.parent_management_group_id
+  parent_management_group_id = azurerm_management_group.parent.id
 }
 
-module "policy_serverless" {
+module "policy_root" {
   source = "github.com/meshcloud/collie-hub//kit/azure/util/azure-policies?ref=ef06c8d"
 
-  policy_path         = "${path.module}/lib"
-  management_group_id = azurerm_management_group.serverless.id
+  policy_path         = "./lib"
+  management_group_id = azurerm_management_group.parent.id
   location            = var.location
 
   template_file_variables = {
     default_location          = "${var.location}"
-    current_scope_resource_id = azurerm_management_group.serverless.id
-    root_scope_resource_id    = azurerm_management_group.serverless.id
+    current_scope_resource_id = azurerm_management_group.parent.id
+    root_scope_resource_id    = azurerm_management_group.parent.id
   }
 }
