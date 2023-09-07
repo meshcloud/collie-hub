1ba53aad937b2f2a5ce7a5eb3a54e40759d7ca93
Florian <flonowa@outlook.com>
2023-09-07 14:43:23 +0200

    chore: terraform fmt

--- kit/azure/landingzones/lz-serverless/documentation.tf
+++ kit/azure/landingzones/lz-serverless/documentation.tf
@@ -2,8 +2,16 @@ output "documentation_md" {
   value = <<EOF
 This landing zone is intended for serverless resources.
 
+## Serverless landingzone
+
+- **${resource.azurerm_management_group.serverless.display_name}** - this is the severless management group 
+
 ### Active Policies
 
+#### Service and Location Restrictions
 
+|Policy|Effect|Description|Rationale|
+|-|-|-|-|
+|[${module.policy_serverless.policy_assignments["Allow-Serverless-Only"].display_name}](https://cloudfoundation.org/maturity-model/tenant-management/cloud-native-landing-zone.html#best-practices-for-designing-and-building-a-cloud-native-landing-zone)|Deny|
 EOF
 }
