2c3cbbdf4abb23da7f9f1413458df08d885f5fba
Johannes Rudolph <jrudolph@meshcloud.io>
2023-09-25 14:34:37 +0200

    refactor: remove the now unused mermaid plugin code

--- kit/foundation/docs/template/docs/.vuepress/theme/plugins/mermaid/README.md
+++
@@ -1,4 +0,0 @@
-This is our own vendored and fixed version of
-https://github.com/eFrane/vuepress-plugin-mermaidjs/pull/78
-
-originally published under MIT License
--- kit/foundation/docs/template/docs/.vuepress/theme/plugins/mermaid/client/MermaidApi.ts
+++
@@ -1,8 +0,0 @@
-export interface MermaidApi {
-  render: (
-    id: string,
-    txt: string,
-    cb?: (svgCode: string, bindFunctions: (element: Element) => void) => void,
-    container?: Element
-  ) => Promise<string>;
-}
