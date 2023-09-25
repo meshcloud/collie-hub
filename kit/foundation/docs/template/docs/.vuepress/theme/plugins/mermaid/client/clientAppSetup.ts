a8955158df478484cb64029d7dc8ba4018a80990
Johannes Rudolph <jrudolph@meshcloud.io>
2023-09-25 14:34:37 +0200

    refactor: remove the now unused mermaid plugin code

--- kit/foundation/docs/template/docs/.vuepress/theme/plugins/mermaid/client/clientAppSetup.ts
+++
@@ -1,7 +0,0 @@
-import { defineClientAppSetup } from '@vuepress/client';
-
-import { setupMermaid } from './composables/setupMermaid';
-
-export default defineClientAppSetup(() => {
-  setupMermaid();
-});
