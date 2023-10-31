d78a4b4d7251381be3e893e08527b0facf9b12f7
florianow <64468897+florianow@users.noreply.github.com>
2023-10-31 17:13:07 +0100

    chore: added output.tf in pam kit

--- kit/azure/pam/outputs.tf
+++ kit/azure/pam/outputs.tf
@@ -1,11 +1,12 @@
-# output "billing_admins_azuread_group_id" {
-#   value = azuread_group.billing_admins.id
-# }
-#
-# output "billing_readers_azuread_group_id" {
-#   value = azuread_group.billing_readers.id
-# }
-#
-# output "security_auditors_azuread_group_id" {
-#   value = azuread_group.security_auditors.id
-# }
+output "billing_admins_members" {
+  value = azuread_group_member.billing_admins.member_object_id
+}
+
+output "billing_readers_members" {
+  value = azuread_group_member.billing_readers.member_object_id
+
+}
+
+output "security_auditors_members" {
+  value = azuread_group_member.security_auditors_members.member_object_id
+}
