29c98d68b3f3732ea66eb0b2588665fa528044b1
florianow <64468897+florianow@users.noreply.github.com>
2023-11-06 07:35:35 +0100

    chore: restructure billing main.tf

--- kit/azure/billing/main.tf
+++ kit/azure/billing/main.tf
@@ -8,17 +8,7 @@ data "azurerm_management_group" "root" {
 
 locals {
   role_definition_name = "Cost Management Contributor"
-  role_definition_link = "https://docs.microsoft.com/en-us/azure/role-based-access-control/built-in-roles#cost-management-contributor"
 }
-
-# data "azuread_users" "billing_users" {
-#   # unfortunately mail_nicknames attribute does not work on our AADs because we don't sync from on-premise
-#   # so we have to use user prinicpal names for lookups
-#   user_principal_names = var.billing_users[*].upn
-# }
-
-# todo: it would probably be a lot prettier to create an AAD group for the billing admins and then assign those permissions
-# as long as this is all in terraform anyway it doesn't matter much though (terraform destroy is supported still)
 
 resource "azuread_group" "billing_admins" {
   display_name     = "cloudfoundation-billing-admins"
@@ -33,7 +23,7 @@ resource "azuread_group" "billing_readers" {
 }
 
 resource "azurerm_role_assignment" "cost_management_contributor" {
-  role_definition_name = local.role_definition_name
+  role_definition_name = "Cost Management Contributor" 
   principal_id         = azuread_group.billing_admins.object_id
   scope                = var.scope
 }
