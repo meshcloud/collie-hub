0c89489b541122d77f0bb8bb25938ea28d1ab33a
Johannes Rudolph <jrudolph@meshcloud.io>
2023-11-06 22:31:00 +0100

    feat: remove azure/admin module
    this has been superseded by the azure/billing module

--- kit/azure/admin/tenant/documentation.tf
+++
@@ -1,8 +0,0 @@
-output "documentation_md" {
-  value = <<EOF
-The id of the AAD Tenant hosting this platform is `${var.aad_tenant_id}`.
-
-${module.billing_admins.documentation}
-
-EOF
-}
--- kit/azure/admin/tenant/main.tf
+++
@@ -1,9 +0,0 @@
-module "billing_admins" {
-  source        = "./billing-admins"
-  aad_tenant_id = var.aad_tenant_id
-  billing_users = var.billing_users
-}
-
-data "azurerm_management_group" "root" {
-  name = var.aad_tenant_id
-}
