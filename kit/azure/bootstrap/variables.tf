2aad7eaa8c595bf3606f0b5fec58e05290a099e4
florianow <64468897+florianow@users.noreply.github.com>
2023-11-07 16:19:29 +0100

    chore: added a platform engineers group as variable

--- kit/azure/bootstrap/variables.tf
+++ kit/azure/bootstrap/variables.tf
@@ -27,3 +27,9 @@ variable "platform_engineers_members" {
     upn   = string,
   }))
 }
+
+variable "platform_engineers_group" {
+  type        = string
+  default     = "cloudfoundation-platform-engineers"
+  description = "the name of the cloud foundation platform engineers group"
+}
