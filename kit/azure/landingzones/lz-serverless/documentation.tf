e84fed99fbd218c0557cdcbb50a00292f7b691b9
florianow <64468897+florianow@users.noreply.github.com>
2023-11-22 13:45:48 +0100

    chore: added more info about serverless lz

--- kit/azure/landingzones/lz-serverless/documentation.tf
+++ kit/azure/landingzones/lz-serverless/documentation.tf
@@ -2,7 +2,7 @@ output "documentation_md" {
   value = <<EOF
 # Serverless Landing Zone
 
-This landing zone is intended for serverless resources.
+A serverless landing zone is a pre-configured environment that is specifically designed for deploying serverless resources.
 
 - **${resource.azurerm_management_group.serverless.display_name}** - this is the severless management group
 
@@ -12,6 +12,6 @@ This landing zone is intended for serverless resources.
 
 |Policy|Effect|Description|Rationale|
 |-|-|-|-|
-|[${module.policy_serverless.policy_assignments["Allow-Serverless-Only"].display_name}](https://cloudfoundation.org/maturity-model/tenant-management/cloud-native-landing-zone.html#best-practices-for-designing-and-building-a-cloud-native-landing-zone)|Deny|
+|[${module.policy_serverless.policy_assignments["Allow-Serverless-Only"].display_name}](https://cloudfoundation.org/maturity-model/tenant-management/cloud-native-landing-zone.html#best-practices-for-designing-and-building-a-cloud-native-landing-zone)|Deny|${module.policy_serverless.policy_assignments["Allow-Serverless-Only"].description}|This policy allows only serverless resources to be deployed.|
 EOF
 }
