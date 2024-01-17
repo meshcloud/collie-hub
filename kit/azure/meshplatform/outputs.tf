9b2a501e12536aa19ccd198a3d8f1dd8551d38b7
Felix Zieger <fzieger@meshcloud.io>
2024-01-17 16:28:21 +0100

    chore: formatting

--- kit/azure/meshplatform/outputs.tf
+++ kit/azure/meshplatform/outputs.tf
@@ -2,7 +2,6 @@ output "meshplatform" {
   value     = module.meshplatform
   sensitive = true
 }
-
 output "replicator_credentials" {
   description = "Replicator Service Principal."
   value       = module.meshplatform.replicator_service_principal
@@ -19,15 +18,6 @@ output "metering_credentials" {
 output "metering_client_secret" {
   description = "Password for Metering Service Principal."
   value       = module.meshplatform.metering_service_principal_password
-  sensitive   = true
-}
-output "uami_blueprint_user_principal" {
-  description = "UAMI Blueprint Assignment Service Principal."
-  value       = module.meshplatform.uami_blueprint_user_principal
-}
-output "uami_blueprint_user_principal_password" {
-  description = "Password for UAMI Blueprint Assignment Service Principal."
-  value       = module.meshplatform.uami_blueprint_user_principal_password
   sensitive   = true
 }
 output "azure_ad_tenant_id" {
