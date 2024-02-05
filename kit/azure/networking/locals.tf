848eb9c1b0f16db19cddca776de2381e82849f70
florianow <64468897+florianow@users.noreply.github.com>
2024-02-05 17:52:39 +0100

    chore: changing network locals

--- kit/azure/networking/locals.tf
+++ kit/azure/networking/locals.tf
@@ -14,44 +14,25 @@ locals {
     log                = []
   }
 
-  # default_nsg_rule = {
-  #   direction                                  = "Inbound"
-  #   access                                     = "Allow"
-  #   protocol                                   = "Tcp"
-  #   description                                = null
-  #   source_port_range                          = null
-  #   source_port_ranges                         = null
-  #   destination_port_range                     = null
-  #   destination_port_ranges                    = null
-  #   source_address_prefix                      = null
-  #   source_address_prefixes                    = null
-  #   source_application_security_group_ids      = null
-  #   destination_address_prefix                 = null
-  #   destination_address_prefixes               = null
-  #   destination_application_security_group_ids = null
-  # }
-
-  default_mgmt_nsg_rules = [
-    {
-      name                       = "allow-load-balancer"
-      source_port_range          = "*"
-      destination_port_range     = "*"
-      source_address_prefix      = "AzureLoadBalancer"
-      destination_address_prefix = "*"
-    },
-    {
-      name                       = "deny-other"
-      access                     = "Deny"
-      protocol                   = "*"
-      source_port_range          = "*"
-      destination_port_range     = "*"
-      source_address_prefix      = "VirtualNetwork"
-      destination_address_prefix = "VirtualNetwork"
-    }
-  ]
+  default_nsg_rule = {
+    direction                                  = "Inbound"
+    access                                     = "Allow"
+    protocol                                   = "Tcp"
+    description                                = null
+    source_port_range                          = null
+    source_port_ranges                         = null
+    destination_port_range                     = null
+    destination_port_ranges                    = null
+    source_address_prefix                      = null
+    source_address_prefixes                    = null
+    source_application_security_group_ids      = null
+    destination_address_prefix                 = null
+    destination_address_prefixes               = null
+    destination_application_security_group_ids = null
+  }
 
   merged_mgmt_nsg_rules = flatten([
-    for nsg in var.management_nsg_rules : merge(nsg)
+    for nsg in var.management_nsg_rules : merge(local.default_nsg_rule, nsg)
   ])
 
   public_ip_map = { for pip in var.public_ip_names : pip => true }
