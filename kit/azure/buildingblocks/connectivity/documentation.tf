93a403e89c89f3999fb09230a56e704d518ca08d
florianow <64468897+florianow@users.noreply.github.com>
2024-06-14 13:57:09 +0200

    chore: remove useless files in connectivity bb

--- kit/azure/buildingblocks/connectivity/documentation.tf
+++
@@ -1,9 +0,0 @@
-output "documentation_md" {
-  value = <<EOF
-# Building Block - Connectivity
-
-The Connectivity building block deploys a managed VNet that's connected to Likvid Bank's network hub.
-This enables on-premise connectivity.
-
-EOF
-}
