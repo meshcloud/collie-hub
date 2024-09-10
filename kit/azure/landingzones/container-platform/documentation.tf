51f21b39fd635223fdb32521f717babc87ece2b7
florianow <64468897+florianow@users.noreply.github.com>
2024-09-10 10:33:38 +0200

    chore: adding new docs for container lz

--- kit/azure/landingzones/container-platform/documentation.tf
+++ kit/azure/landingzones/container-platform/documentation.tf
@@ -1,18 +1,30 @@
 output "documentation_md" {
   value = <<EOF
-# Container Platform Landing Zone
 
-A Container Platform Landing Zone is a pre-configured infrastructure setup designed to support the deployment of containerized serverless applications. This landing zone is designed to provide a secure and compliant environment for running containerized workloads on Azure Kubernetes Service (AKS).
+# 🚢 Container Platform Landing Zone
 
-- **${resource.azurerm_management_group.container_platform.display_name}** - this is the Container Platform management group
+A Container Platform Landing Zone is a pre-configured infrastructure setup designed to support the deployment of containerized serverless applications. This landing zone is designed to provide a secure and compliant environment for running containerized workloads on Azure Kubernetes Service (AKS). 🚀
 
-### Active Policies
+## 📂 Management Group Structure
+
+```md
+`${resource.azurerm_management_group.container_platform.display_name}` management group for cloud-native landing zone 🚢 
+  ├── `${resource.azurerm_management_group.dev.display_name}` management group for development workloads 🛠️
+  │  └── *application team subscriptions* 📚
+  └── `${resource.azurerm_management_group.prod.display_name}` management group for production workloads 🏭
+     └── *application team subscriptions* 📚
+```
+
+## 🛡️ Active Policies
 
 |Policy|Effect|Description|Rationale|
 |-|-|-|-|
 |${module.policy_container_platform.policy_assignments["K8S-Security-Baseline"].display_name}|Audit|${module.policy_container_platform.policy_assignments["K8S-Security-Baseline"].description}|This initiative enforces several security best practices for Kubernetes pods, such as running containers as a non-root user and not allowing privilege escalation. These practices help to minimize the attack surface of your Kubernetes workloads and protect against common security threats.|
 
+## 📚 References
+
 - [CFMM container-platform-landing-zone](https://cloudfoundation.org/maturity-model/tenant-management/container-platform-landing-zone.html)
 - [Policy Set K8S-Security-Baseline](https://www.azadvertizer.net/azpolicyinitiativesadvertizer/a8640138-9b0a-4a28-b8cb-1666c838647d.html)
+
 EOF
 }
--- kit/azure/landingzones/container-platform/main.tf
+++ kit/azure/landingzones/container-platform/main.tf
@@ -1,6 +1,24 @@
 resource "azurerm_management_group" "container_platform" {
-  display_name               = var.lz-container-platform
+  display_name               = var.name
   parent_management_group_id = var.parent_management_group_id
+}
+
+resource "azurerm_management_group" "dev" {
+  display_name               = "${var.name}-dev"
+  parent_management_group_id = azurerm_management_group.container_platform.id
+
+  lifecycle {
+    ignore_changes = [subscription_ids]
+  }
+}
+
+resource "azurerm_management_group" "prod" {
+  display_name               = "${var.name}-prod"
+  parent_management_group_id = azurerm_management_group.container_platform.id
+
+  lifecycle {
+    ignore_changes = [subscription_ids]
+  }
 }
 
 module "policy_container_platform" {
