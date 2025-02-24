cef63f79c74dcab07a064868a972211872494696
Florian Nowarre <fnowarre@meshcloud.io>
2025-02-24 16:18:16 +0100

    chore: change version back
    chore: change version back

--- kit/azure/buildingblocks/budget-alert/buildingblock/versions.tf
+++ kit/azure/buildingblocks/budget-alert/buildingblock/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.108.0"
+      version = "~> 3.116.0"
     }
     time = {
       source  = "hashicorp/time"
