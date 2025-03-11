a760f21323ca90c325b6d8e72a235895c7e8439d
Florian Nowarre <fnowarre@meshcloud.io>
2025-03-11 12:48:38 +0100

    chore: adding postgres kit
    chore: adding postgres kit
    
    chore: adding postgres kit
    
    chore: adding postgres kit

---
+++ kit/azure/buildingblocks/postgresql/buildingblock/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.22.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
