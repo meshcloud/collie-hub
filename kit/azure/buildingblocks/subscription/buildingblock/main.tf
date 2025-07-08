1bce4cccb5f07e3715de961eccbfc5b4902ac7c5
florianow <64468897+florianow@users.noreply.github.com>
2025-07-08 13:16:07 +0200

    Update main.tf

--- kit/azure/buildingblocks/subscription/buildingblock/main.tf
+++ kit/azure/buildingblocks/subscription/buildingblock/main.tf
@@ -2,12 +2,12 @@ data "azurerm_subscription" "current" {
 }
 
 # workaround for https://github.com/hashicorp/terraform-provider-azurerm/issues/23014
-resource "terraform_data" "subscription_name" {
-  provisioner "local-exec" {
-    when    = create
-    command = "az account subscription rename --id ${data.azurerm_subscription.current.subscription_id} --name ${var.subscription_name}"
-  }
-}
+#resource "terraform_data" "subscription_name" {
+#  provisioner "local-exec" {
+#    when    = create
+#    command = "az account subscription rename --id ${data.azurerm_subscription.current.subscription_id} --name ${var.subscription_name}"
+#  }
+#}
 
 
 // control placement in the LZ hierarchy
